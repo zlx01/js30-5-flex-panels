b61edfd8371ac8968426bc99e0fe6ba0fc27409a
zlx01 <761451281@qq.com>
2022-08-03 23:34:26 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open')
+}
+
+function toggleActive(e) {
+  // Safari transitionend event.propertyName === flex
+  // Chrome + FF transitionend event.propertyName === flex-grow
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
